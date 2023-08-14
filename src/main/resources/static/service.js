c812b39ed89dbd5ae3a940e959b6740aa7bc3498
deanOfWalls <deanofwalls@gmail.com>
2023-08-14 02:25:44 -0400

    reverting changes to prepare for React integreation

--- src/main/resources/static/service.js
+++ src/main/resources/static/service.js
@@ -1,71 +1,177 @@
-import React, { useState } from 'react';
-import $ from 'jquery';
-
 // Function to update the JSON data in the textarea
 function updateJsonDisplay(jsonData) {
-  const jsonDisplayElement = document.getElementById("json-display");
-  jsonDisplayElement.value = JSON.stringify(jsonData, null, 2); // Beautify JSON for better display
+    const jsonDisplayElement = document.getElementById("json-display");
+    jsonDisplayElement.value = JSON.stringify(jsonData, null, 2); // Beautify JSON for better display
 }
 
 class Person {
-  constructor(id, firstName, lastName, birthDate) {
-    this.id = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.birthDate = birthDate;
-  }
+    constructor(id, firstName, lastName, birthDate) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
 }
 
-const DataService = () => {
-  // State variables
-  const [jsonData, setJsonData] = useState('');
+function create(event) {
+    event.preventDefault(); //prevent default submission since asynchronous
 
-  // Event handler for form submission - create
-  const create = async (event) => {
-    event.preventDefault(); // Prevent default form submission
+    const personIdElement = document.getElementById("person-id");
+    const firstNameElement = document.getElementById("first-name");
+    const lastNameElement = document.getElementById("last-name");
+    const birthDateElement = document.getElementById("birth-date");
 
-    // ... (Rest of your create function)
+    const personIdValue = personIdElement.value;
+    const firstNameValue = firstNameElement.value;
+    const lastNameValue = lastNameElement.value;
+    const birthDateValue = birthDateElement.value;
+    const person = new Person(personIdValue, firstNameValue, lastNameValue, birthDateValue);
 
-    try {
-      const response = await $.ajax({
-        type: 'POST',
+    const personData = JSON.stringify({
+        id: personIdValue,
+        firstName: firstNameValue,
+        lastName: lastNameValue,
+        birthDate: birthDateValue,
+    });
+
+    $.ajax({
+        type: "POST",
         crossDomain: true,
         headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
         },
-        url: '/create',
+        url: "/create",
         data: personData,
-        dataType: 'JSON',
-      });
+        dataType: "JSON",
+        success: function (response) {
+            updateJsonDisplay(response); // Display the JSON data in the textarea after the request is successful
+        },
+        error: function (error) {
+            updateJsonDisplay("An error occurred during the create request.");
+        }
+    });
+}
 
-      updateJsonDisplay(response); // Update JSON data in the textarea
+async function readAll(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    try {
+        const response = await $.ajax({
+            type: "GET",
+            crossDomain: true,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            url: "/readAll",
+        });
+
+        // Display the JSON data in the textarea
+        updateJsonDisplay(response);
     } catch (error) {
-      updateJsonDisplay('An error occurred during the create request.');
+        updateJsonDisplay("An error occurred during the readAll request.");
     }
-  };
+}
 
-  // ... (Rest of your event handlers)
+async function readById(event) {
+    event.preventDefault();
+    const personIdElement = document.getElementById("person-id");
+    const personIdValue = personIdElement.value;
 
-  return (
-    <div>
-      <div className="header">
-        <h1>Your Header</h1>
-      </div>
+    try {
+        const response = await $.ajax({
+            type: "GET",
+            crossDomain: true,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            url: "/read/" + personIdValue,
+        });
 
-      {/* Render your form here with input fields and buttons */}
-      <div className="form-container-container">
-        <form onSubmit={create}>
-          {/* Your input fields */}
-          {/* Your submit button */}
-        </form>
-      </div>
+        // Display the JSON data in the textarea instead of using stringify popup
+        updateJsonDisplay(response);
+    } catch (error) {
+        updateJsonDisplay("An error occurred during the readById request.");
+    }
+}
 
-      {/* Display JSON data in the textarea */}
-      <textarea id="json-display" value={jsonData} readOnly></textarea>
-    </div>
-  );
-};
+async function update(event) {
+    event.preventDefault();
 
-export default DataService;
+    const personIdElement = document.getElementById("person-id");
+    const personIdValue = personIdElement.value;
+
+    const firstNameElement = document.getElementById("first-name");
+    const lastNameElement = document.getElementById("last-name");
+    const birthDateElement = document.getElementById("birth-date");
+
+    const firstNameValue = firstNameElement.value;
+    const lastNameValue = lastNameElement.value;
+    const birthDateValue = birthDateElement.value;
+
+    console.log("Updating Person Data:", {
+        id: personIdValue,
+        firstName: firstNameValue,
+        lastName: lastNameValue,
+        birthDate: birthDateValue,
+    });
+
+    try {
+        const response = await $.ajax({
+            type: "PUT",
+            crossDomain: true,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            url: "/update/" + personIdValue,
+            data: JSON.stringify({
+                id: personIdValue,
+                firstName: firstNameValue,
+                lastName: lastNameValue,
+                birthDate: birthDateValue,
+            }),
+        });
+
+        console.log("Update Response:", response);
+        // Display the JSON data in the textarea instead of using stringify popup
+        updateJsonDisplay(response);
+    } catch (error) {
+        console.error("Update Error:", error);
+        updateJsonDisplay("An error occurred during the update request.");
+    }
+}
+
+async function deleteThing(event) {
+    event.preventDefault();
+
+    const personIdElement = document.getElementById("person-id");
+    const personIdValue = personIdElement.value;
+
+    try {
+        const response = await $.ajax({
+            type: "DELETE",
+            crossDomain: true,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            url: "/delete/" + personIdValue,
+        });
+
+        // Display the JSON data in the textarea instead of using stringify popup
+        updateJsonDisplay(response);
+    } catch (error) {
+        updateJsonDisplay("An error occurred during the deleteThing request.");
+    }
+}
+
+
+
